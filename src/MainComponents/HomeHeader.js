e5a11ca4336d8909f4596671f2036a1ef6734c93
kimberlybone <k_bone@uncg.edu>
2020-04-01 19:11:43 -0400

    header css changed

--- src/MainComponents/HomeHeader.js
+++ src/MainComponents/HomeHeader.js
@@ -5,8 +5,10 @@ function HomeHeader() {
   return(
     <div className="home-header">
       <img className="mm-image" src="IMG_1021.JPG" alt="mm" />
-      <div class="mm-home-head"><text className="mm-home-num">6</text> Black Women from </div>
-      <div class="mm-home-head2"><text className="mm-home-num">6</text> Different Walks of Life</div>
+      <div className="mm-home-head-texts">
+        <p class="mm-home-head"><text className="mm-home-num">6</text> Black Women from </p>
+        <p class="mm-home-head2"><text className="mm-home-num">6</text> Different Walks of Life</p>
+      </div>
     </div>
   )
 }
