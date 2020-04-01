07615a085daca651769f2029ffc7cba74d315e91
kimberlybone <k_bone@uncg.edu>
2020-04-01 18:23:43 -0400

    story card positiong fixed

--- src/ChildComponents/Story.js
+++ src/ChildComponents/Story.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import '../MainComponents.css'
+import '../ChildComponents.css'
 
 function Story(props) {
   const { item } = props
   return(
-    <div>
+    <div className='mm-home-story'>
       <p className='mm-home-name'>{ item.name } </p>
       <img className='mm-home-gifs' src={ item.imgSrc }/>
-      <p className='mm-home-story'>{ item.bio } </p>
+      <p className='mm-home-intro'>{ item.intro }</p>
+      <p className='mm-home-bio'>"...{ item.bio }" </p>
     </div>
   )
 
--- src/MainComponents/OurStories.js
+++ src/MainComponents/OurStories.js
@@ -11,37 +11,43 @@ class OurStories extends React.Component {
         id: 0,
         name: "Caci",
         imgSrc: "Caci-anim.gif",
-        bio: "I want Mocha Moves to become a breeding ground for safe spaces for women of all backgrounds. In order to strengthen personal relationships as they are necessary for a healthy life."
+        intro: "I want Mocha Moves to",
+        bio: "become a breeding ground for safe spaces for women of all backgrounds. In order to strengthen personal relationships as they are necessary for a healthy life."
       },
       {
         id: 1,
         name: "Cam",
         imgSrc: "Cam-anim.gif",
-        bio: "I would love to see Mocha Moves grow and flourish to become a prominent figure in Black culture by inspiring Black women, chnaging narratives, and expanding into business opportunities. Throughout this experience, my hope is the six of us continue to strengthen and deepen our connections."
+        intro: "I would love to see Mocha Moves",
+        bio: "grow and flourish to become a prominent figure in Black culture by inspiring Black women, chnaging narratives, and expanding into business opportunities. Throughout this experience, my hope is the six of us continue to strengthen and deepen our connections."
       },
       {
         id: 2,
         name: "Dee",
         imgSrc: "Dee-anim.gif",
-        bio: "I hope Mocha Moves gives women around the world exactly what I needed way back when, and still need today..inspiration. Whatever you want in life exists, so go get it. Love yourself, and never settle."
+        intro: "I hope Mocha Moves",
+        bio: "gives women around the world exactly what I needed way back when, and still need today..inspiration. Whatever you want in life exists, so go get it. Love yourself, and never settle."
       },
       {
         id: 3,
         name: "Kim",
         imgSrc: "KB-anim.gif",
-        bio: "My goal for Mocha Moves is to be an inspiration to women, especially those of color. I want to make sure they know that we can do anything we set our minds to. As Michelle Obama said there is no limit to what we as women can accomplish."
+        intro: "I would love for Mocha Moves",
+        bio: "to be an inspiration to women, especially those of color. I want to make sure they know that we can do anything we set our minds to. As Michelle Obama said there is no limit to what we as women can accomplish."
       },
       {
         id: 4,
         name: "Randi",
         imgSrc: "Randi-anim.gif",
-        bio: "My goal for Mocha Moves is to share this loving energy with everyone. My friends and I have a romance that I think women (especially women of color) are longing to see and be apart of. With this brand, I hope to create a community."
+        intro: "My goal for Mocha Moves is",
+        bio: "to share this loving energy with everyone. My friends and I have a romance that I think women (especially women of color) are longing to see and be apart of. With this brand, I hope to create a community."
       },
       {
         id: 5,
         name: "Tre",
         imgSrc: "Tre-anim.gif",
-        bio: "My goal for Mocha Moves is to create a mirror in which girls and women see themselves. To show that no matter who you are, there's a pillar of light and a friend in a dark space. Even if only one person is touched, I will feel accomplished."
+        intro: "My goal for Mocha Moves is",
+        bio: "to create a mirror in which girls and women see themselves. To show that no matter who you are, there's a pillar of light and a friend in a dark space. Even if only one person is touched, I will feel accomplished."
       }
     ]
   }
